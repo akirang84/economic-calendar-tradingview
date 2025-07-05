13d8b1f429b96996545ff18e9cf6004a8f29fbbc
Minh Nguyen <minh.nguyen1@episource.com>
2025-07-05 20:07:52 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import {CalendarModule} from "./calendar/calendar.module";
 
 async function bootstrap() {
     const app = await NestFactory.create(CalendarModule);
-    await app.listen(process.env.PORT || 3000, "0.0.0.0");
+    await app.listen(process.env.PORT || 8889, "0.0.0.0");
 }
 bootstrap();
